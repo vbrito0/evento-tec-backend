ba9a7e71f48cd21d749ae79dc36ed678e0b4de10
Victor Brito <victor.silva1189@gmail.com>
2024-08-21 20:40:41 -0300

    Atualização test

--- src/test/java/com/eventostec/api/ApiApplicationTests.java
+++ src/test/java/com/eventostec/api/ApiApplicationTests.java
@@ -1,15 +1,10 @@
 package com.eventostec.api;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class ApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
